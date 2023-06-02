26cdb4cc81a070c8383a40ab3e6420eb3420a00d
DannCarlo <46589165+DannCarlo@users.noreply.github.com>
2023-06-02 19:22:33 +0800

    Add files via upload

--- js/sendemail.js
+++ js/sendemail.js
@@ -27,7 +27,7 @@
                 if(validateForm()){
                  e.preventDefault();
                   $.ajax({type: "POST",
-                          url: "http://www.yourdomain.com/send-email.php",
+                          url: "https://www.danncarlo.com/send-email.php",
                           data:$("#form1").serialize(),
                           success:function(result){
                           $("#successmsg").html(result);
