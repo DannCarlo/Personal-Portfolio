59f190ad01ac085cbfc9c0827803656959cf1121
DannCarlo <46589165+DannCarlo@users.noreply.github.com>
2024-05-27 20:32:56 +0800

    Add files via upload

--- js/sendemail.js
+++ js/sendemail.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  
+  document.querySelector("form").addEventListener("submit", handleSubmit);
+});
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  if (!validateForm()) {
     const myForm = event.target;
     const formData = new FormData(myForm);
     
@@ -10,9 +14,46 @@ $(document).ready(function() {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-      .then(() => alert("Thank you for your submission"))
-      .catch((error) => alert(error));
-  };
-  
-  document.querySelector("form").addEventListener("submit", handleSubmit);
-});
+    .then(() => alert("Thank you for your submission."))
+    .catch((error) => alert(error));
+    clearInputValue();
+  }
+  else {
+    alert("Please answer the form properly.")
+  }
+};
+
+function validateForm() {
+  "use strict";
+  var title = $("#name").val();
+  var err = true;
+  if (title == "" || title == null) {
+      $("#name").addClass("validation");
+
+      var err = false;
+  } else {
+      $("#name").removeClass("validation");
+  }
+  var email = $("#email").val();
+  if (!(/(.+)@(.+){2,}\.(.+){2,}/.test(email))) {
+      $("#email").addClass("validation");
+
+      var err = false;
+  } else {
+      $("#email").removeClass("validation");
+  }
+  var title = $("#message").val();
+  if (title == "" || title == null) {
+      $("#message").addClass("validation");
+      var err = false;
+  } else {
+      $("#message").removeClass("validation");
+  }
+  return err;
+}
+
+function clearInputValue() {
+  $("#name").val("");
+  $("#email").val("");
+  $("#message").val("");
+}
